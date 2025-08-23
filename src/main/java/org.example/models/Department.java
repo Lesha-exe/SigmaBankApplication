6edcc493d2a952cadaf176929223d8c083dcc617
aleksei_iaroshevich <leaha007.yaroshevich@gmail.com>
2025-08-23 22:32:18 +0300

    Parsers realisation

---
+++ src/main/java/org.example/models/Department.java
@@ -0,0 +1,8 @@
+package org.example.models;
+
+import java.util.List;
+
+public class Department {
+    private Manager manager;
+    private List<Employee> employeeList;
+}
--- src/main/java/org.example/services/EmployeeParser.java
+++ src/main/java/org.example/services/EmployeeParser.java
@@ -2,16 +2,39 @@ package org.example.services;
 
 import lombok.Builder;
 import org.example.models.Employee;
+import org.example.models.Manager;
 import org.example.models.Worker;
+import org.example.models.WorkerWithIncorrectData;
 
-public class EmployeeParser implements WorkerParser{
+public class EmployeeParser implements WorkerParser {
     @Override
     public Worker parse(String[] workerData) {
         Employee.EmployeeBuilder employeeBuilder = Employee.builder();
-        employeeBuilder.id(Integer.parseInt(workerData[1]));
+        try {
+            employeeBuilder.id(Integer.parseInt(workerData[1]));
+            employeeBuilder.salary(Integer.parseInt(workerData[3]));
+            employeeBuilder.managerId(Integer.parseInt(workerData[4]));
+        } catch (NumberFormatException exception) {
+            return new WorkerWithIncorrectData(workerData);
+        }
         employeeBuilder.name(workerData[2]);
-        employeeBuilder.salary(Integer.parseInt(workerData[3]));
-        employeeBuilder.managerId(Integer.parseInt(workerData[4]));
-        return employeeBuilder.build();
+        Employee employee = employeeBuilder.build();
+        if (!isValid(employee)) {
+            return new WorkerWithIncorrectData(workerData);
+        }
+        return employee;
+    }
+
+    private boolean isValid(Employee employee) {
+        if (employee.getId() < 0 || employee.getId().equals(null)) {
+            return false;
+        } else if (employee.getSalary() < 0 || employee.getSalary().equals(null)) {
+            return false;
+        }else if (employee.getManagerId() < 0 || employee.getManagerId().equals(null)) {
+            return false;
+        } else if (employee.getName().equals(null)) {
+            return false;
+        }
+        return true;
     }
 }
