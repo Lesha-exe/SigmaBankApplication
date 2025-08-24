1ee2eabad2a79f98f3f0581c0eb429510b6c4067
aleksei_iaroshevich <leaha007.yaroshevich@gmail.com>
2025-08-24 18:06:05 +0300

    Department services realisation(not ready)

--- src/main/java/org.example/WorkerDataApplication.java
+++ src/main/java/org.example/WorkerDataApplication.java
@@ -2,21 +2,29 @@ package org.example;
 
 import lombok.RequiredArgsConstructor;
 import org.example.models.AppArguments;
+import org.example.models.Department;
 import org.example.services.ArgumentsReader;
+import org.example.services.DepartmentServices;
 import org.example.services.WorkerData;
 import org.example.services.WorkerDataReader;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class WorkerDataApplication {
     private final ArgumentsReader argumentsReader;
     private final WorkerDataReader workerDataReader;
+    private final DepartmentServices departmentServices;
 
     public void run(String[] args) {
         try {
             AppArguments appArguments = argumentsReader.readArguments(args);
             WorkerData workerData = workerDataReader.readWorkers();
+            List<Department> departments = departmentServices.
+                    createDepartment(workerData.getWorkersWithCorrectData(), appArguments);
+
         } catch (Exception exception){
             System.out.println(exception.getMessage());
         }
--- src/main/java/org.example/services/DepartmentServices.java
+++ src/main/java/org.example/services/DepartmentServices.java
@@ -1,4 +1,43 @@
 package org.example.services;
 
+import org.example.models.*;
+import org.example.outputSettings.OrderType;
+import org.example.outputSettings.SortType;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class DepartmentServices {
+    public List<Department> createDepartment (List<Worker> workers, AppArguments appArguments){
+        List<Manager> managers = workers.stream().
+                filter(worker -> worker instanceof Manager).
+                map(worker -> (Manager) worker).
+                toList();
+        List<Employee> employees = workers.stream().
+                filter(worker -> worker instanceof Employee).
+                map(worker -> (Employee) worker).
+                toList();
+        sortEmployees(employees, appArguments.getSortType(), appArguments.getOrder());
+        Map<Integer, Manager> managerMap = managers.stream().
+                collect(Collectors.toMap(Manager::getId, manager -> manager));
+        Map<Integer, List<Employee>> employeeMap = employees.stream().
+                collect(Collectors.groupingBy(Employee::getManagerId));
+
+        List<Department> departments =
+        return null;
+    }
+
+    public void saveDepartments(List<Department> departments){
+    }
+
+    private List<Employee> sortEmployees(List<Employee> employees,
+                                                       SortType sortType, OrderType orderType){
+        if(sortType.equals(SortType.SALARY)){
+            return employees.stream().sorted(Comparator.comparing(Employee::getSalary)).toList();
+        }
+        return employees;
+    }
+
 }
