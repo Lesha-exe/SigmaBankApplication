0668bdbb6e006e1f833ce17c023ce277cf5b3df2
aleksei_iaroshevich <leaha007.yaroshevich@gmail.com>
2025-08-30 20:45:34 +0300

    Main class changes

--- src/main/java/ru/korona/task/SigmaBankApp.java
+++ src/main/java/ru/korona/task/SigmaBankApp.java
@@ -1,6 +1,7 @@
 package ru.korona.task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,7 @@ import static org.springframework.boot.Banner.Mode.LOG;
 @SpringBootApplication
 public class SigmaBankApp implements CommandLineRunner {
     private static final Logger LOG = LoggerFactory.getLogger(SigmaBankApp.class);
+    @Autowired
     private WorkerDataApplication workerDataApplication;
     public static void main(String[] args) {
         LOG.info("STARTING THE APPLICATION");
